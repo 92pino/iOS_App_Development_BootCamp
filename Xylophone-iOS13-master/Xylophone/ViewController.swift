fb46a3ecad214db171582b2aa7fe27c623f5ea8d
92pino <jjb8382@gmail.com>
2020-06-07 17:59:50 +0900

    ✏️ Udemy BootCamp Xylophone Project
    - AVFoundation - AVAudioPlayer

---
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+  var player: AVAudioPlayer?
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
+
+
+  @IBAction func keyPressed(_ sender: UIButton) {
+    guard let title = sender.currentTitle else {
+      return
+    }
+    
+    print(title)
+    
+    playSound(title)
+  }
+  
+  func playSound(_ title: String) {
+    guard let url = Bundle.main.url(forResource: title, withExtension: "wav") else { return }
+    
+    do {
+      try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+      try AVAudioSession.sharedInstance().setActive(true)
+      player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+      guard let player = player else { return }
+      player.play()
+    } catch let error {
+      print(error.localizedDescription)
+    }
+  }
+
+}
+
